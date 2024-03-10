8ac600fe07dffa4c83a461e54012c2c5ad5117f3
Mattia Castellani <Castellani1996@gmail.com>
2024-03-10 20:41:08 +0100

    start printing something

--- src/node/image.ts
+++ src/node/image.ts
@@ -1,80 +1,61 @@
-import { Image, createCanvas, loadImage, registerFont } from 'canvas'
+import { ImageData, createCanvas, loadImage, registerFont } from 'canvas'
 import * as fs from 'fs/promises' 
 
-const PRINT_WIDTH = 384
+export const PRINT_WIDTH = 384
 
 export class CatImage {
-    private constructor(public buff: Buffer, private img: Image) { }
+    private constructor(private buff: Buffer, public imageData: ImageData) { }
 
-    // static async drawText(input: string): Promise<CatImage> {
-    //     registerFont('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', { family: 'DejaVu Sans' })
+    static async drawText(input: string): Promise<CatImage> {
+        registerFont('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', { family: 'DejaVu Sans' })
     
-    //     const canvas = createCanvas(PRINT_WIDTH, 200)
-    //     const ctx = canvas.getContext('2d')
-    //     ctx.font = '30px DejaVuSans.ttf'
-    //     ctx.fillText(input, 10, 190)
+        const canvas = createCanvas(PRINT_WIDTH, 200)
+        const ctx = canvas.getContext('2d')
+        ctx.font = '12px DejaVuSans.ttf'
+        ctx.fillText(input, 10, 190)
     
-    //     const buff: Buffer = canvas.toBuffer("image/png")
-    //     return new CatImage(buff, img)
-    // }
+        const imgData: ImageData = ctx.getImageData(0, 0, PRINT_WIDTH, 200)
+        const data = imgData.data
+        
+        for(let i = 0; i < ImageData.length; i += 4) {
+            const avg = (data[i] + data[i+1] + data[i+2])
+            data[i] = avg
+            data[i+1] = avg
+            data[i+2] = avg
+        }
+
+        ctx.putImageData(imgData, 0, 0)
+        const new_imgData = ctx.getImageData(0, 0, PRINT_WIDTH, 200)
+        const buff: Buffer = canvas.toBuffer()
+        return new CatImage(buff, new_imgData)
+    }
     
     static async loadFromPath(path: string): Promise<CatImage> {
         const myimg = await loadImage(path)
         const canvas = createCanvas(PRINT_WIDTH, 200)
         const ctx = canvas.getContext('2d')
+
         ctx.drawImage(myimg, 0 , 0, PRINT_WIDTH, 200)
         
-        const imgData = ctx.getImageData(0, 0, PRINT_WIDTH, 200)
+        const imgData: ImageData = ctx.getImageData(0, 0, PRINT_WIDTH, 200)
+        
         const data = imgData.data
-
-        const bin_image = []
-
-        for (let i = 0; i < data.length; i += 4) {
-            const isWhite = data[i] === 255 && data[i+1] === 255 && data[i+2] === 255
-            bin_image.push(isWhite ? 0 : 1)
-        //     const avg = (data[i] + data[i + 1] + data[i + 2]) / 3
-        //     data[i] = avg // red
-        //     data[i + 1] = avg // green
-        //     data[i + 2] = avg // blue
+        
+        for(let i = 0; i < ImageData.length; i += 4) {
+            const avg = (data[i] + data[i+1] + data[i+2])
+            data[i] = avg
+            data[i+1] = avg
+            data[i+2] = avg
         }
-        // ctx.putImageData(imgData, 0, 0)
 
-        // const buff: Buffer = canvas.toBuffer()
-        // const img: Image = await loadImage(buff)
-        // const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        // const data = imageData.data;
-        // for (let i = 0; i < data.length; i += 4) {
-        //     const avg = (data[i] + data[i + 1] + data[i + 2]) / 3
-        //     data[i] = avg // red
-        //     data[i + 1] = avg // green
-        //     data[i + 2] = avg // blue
-        // }
-        // ctx.putImageData(imageData, 0, 0)
-
-        const buff = Buffer.from(bin_image)
-
-        await fs.writeFile('test.png', buff)
-
-        return new CatImage(buff, myimg)
-    }
-
-    public getRows(): Buffer[] {
-        const rows: Buffer[] = []
-        let pixel: number = 0
-        let img_lenght = this.buff.length / PRINT_WIDTH 
-        for (let line = 0; line < img_lenght; line += 1) {
-            rows.push(this.buff.subarray(pixel, pixel + PRINT_WIDTH))
-            pixel += PRINT_WIDTH
-        } 
-        return rows
+        ctx.putImageData(imgData, 0, 0)
+        const new_imgData = ctx.getImageData(0, 0, PRINT_WIDTH, 200)
+        const buff: Buffer = canvas.toBuffer()
+        return new CatImage(buff, new_imgData)
     }
 
     public async save(): Promise<void> {
         return await fs.writeFile('./img.png', this.buff)
-    }
-
-    public async resize() {
-
     }
 }
 
--- src/print.mjs
+++ src/print.mjs
@@ -10,23 +10,3 @@ await printer.sendImage('./assets/catprinter.jpg', true)
 await printer.disconnect()
 
 process.exit()
-
-// import * as cmd from "../dist/commands.js"
-
-// function valueToByteArray(value, bytesLength) {
-//     const bytesArray = [];
-//     for (let i = bytesLength - 1; i >= 0; i--) {
-//         const conjunctionVal = 0xFF << (i * 8);
-//         const shift = i * 8;
-//         bytesArray.push((value & conjunctionVal) >> shift);
-//     }
-//     return bytesArray;
-// }
-
-// // Example usage:
-// const myValue = 81; // Some numeric value
-// const myBytes = valueToByteArray(myValue, 2); // Creates a 2-byte array
-
-// // console.log(myBytes)
-// console.log(cmd.CMD_GET_DEV_STATE)
-
