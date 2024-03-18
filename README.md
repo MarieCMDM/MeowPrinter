ad89e9d1e4e8be14b7c044015a06d184e7ed29d7
Mattia Castellani <Castellani1996@gmail.com>
2024-03-18 14:38:20 +0100

    Changed image library

--- README.md
+++ README.md
@@ -1,20 +1,49 @@
 # Node-Catprinter
-Library to print with cat-shaped or other thermal printers that use this protocol over bluetooth 
+Nodejs library to print with cat-shaped or other thermal printers that use this protocol over bluetooth 
 
-### EXAMPLES
-##### CatPrinter
+based on [NaitLee/kitty-printer](https://github.com/NaitLee/kitty-printer) implementation ive made this repo for more general pourpose
+
+## Install
+clone this repo, then 
+```bash
+cd ./meowprinter
+npm install
+```
+
+## API
+`async printImage('path_or_url')`  
+
+`async printText('some text here')`  
+
+`async drawSeparator(optional? separator height)` 
+
+`setStrenght(value)`
+
+`setPrintingSpeed(value)`
+
+`setAlignment(alignment)`
+
+`getPrinterStatus()`
+
+`retractPaper(lines)`
+
+`feedPaper(lines)`
+
+`disconnect()`
+
+
+## EXAMPLE
 ```javascript
 import { CatPrinter } from "cat_device."
 import { BluetoothAdapter } from "ble_adapter"
 
-
 const adapter = new BluetoothAdapter()
-await adapter.scan('54:5d:f8:3a:24:de', 30000)
+await adapter.scan('MY_MAC_ADDRESS')
 
 const printer = new CatPrinter(adapter)
 
 await printer.printImage('./assets/catprinter.jpg')
 await printer.printText('Some text here, some other will go in new line')
 await printer.disconnect()
-
 ```
+
--- demo.mjs
+++ demo.mjs
@@ -1,13 +1,36 @@
-import { CatPrinter } from "./dist/catprinter/cat_device.js"
-import { BluetoothAdapter } from "./dist/catprinter/ble_adapter.js"
+import { CatPrinter } from "./dist/cat_device.js"
+import { BluetoothAdapter } from "./dist/ble_adapter.js"
 
 const adapter = new BluetoothAdapter()
 await adapter.scan('54:5d:f8:3a:24:de', 30000)
+// await adapter.scan('cc:10:24:22:f6:86', 30000)
 
 const printer = new CatPrinter(adapter)
 
-await printer.printImage('./assets/catprinter.jpg')
-await printer.printText('Some text here, some other will go in new line')
+printer.loadFont({path: './assets/Pacifico.ttf', name: 'Pacifico'})
+printer.newText()
+
+printer.addText('Hello, World', {font: '', font_size: 24, bold: true, alignment: 'center' })
+printer.newLine()
+printer.addText('qua stampo del corsivo!', {font: '', font_size: 20, italic: true })
+printer.newLine()
+printer.addText('qua stampo del testo lungo per mostrare che va a capo da solo', {font: '', font_size: 20})
+printer.addText('e ora sto per scrivere una parola', {font: '', font_size: 20})
+printer.addText('sottolineata', {font: '', font_size: 20, underline: true})
+printer.addText('e anche quello funziona', {font: 'Pacifico', font_size: 20})
+
+await printer.printText()
+
+printer.newText()
+
+printer.addText('Ora Stampo un immagine', {font: '', font_size: 48, alignment: "center", bold: true})
+
+await printer.printText()
+
+// await printer.printImage('./assets/catprinter.jpg')
+
+await printer.feedPaper(150)
+
 await printer.disconnect()
 
 process.exit()
